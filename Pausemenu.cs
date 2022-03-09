a4fbcf4e5e86e27f0297a030c2e4cee0307decbf
SahilatGit <91673215+SahilatGit@users.noreply.github.com>
2022-03-09 22:16:19 +0530

    Add files via upload

---
+++ Pausemenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausemenu : MonoBehaviour
+{
+  public static bool GameisPaused = false;
+  public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameisPaused)
+            {
+                Resume();
+            }   else
+            {
+                Pause();
+            }
+        }     
+    }
+
+    public void Resume(){
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+
+    }
+
+    void Pause(){
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+    }
+
+    public void LoadMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    public void QuitGame(){
+        Debug.Log("Quitting Game..");
+        Application.Quit();
+    }
+}
