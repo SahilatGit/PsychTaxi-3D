bcdd22cee167fb3538f3f8ab81f5b7432972ec17
SahilatGit <91673215+SahilatGit@users.noreply.github.com>
2022-03-09 22:15:15 +0530

    Add files via upload

---
+++ scripts/playercollision.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class playercollision : MonoBehaviour
+{
+
+    public movement playermove;
+
+    void OnCollisionEnter(Collision collisioninfo)
+    {
+        if(collisioninfo.collider.tag == "obstacle")
+        {
+            playermove.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ scripts/score.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public Transform player;
+    public Text Highscore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Highscore.text = player.position.z.ToString("0");    
+    }
+}
